f217ea9234ef655783d0e51af6974917bf744e8d
MisterSyntax <asyriala@gmail.com>
2017-07-18 10:42:56 -0400

    fixed year pagination

--- src/components/CalendarFooter/index.js
+++ src/components/CalendarFooter/index.js
@@ -6,13 +6,30 @@ const CalendarHeader = (props) => {
     let month = props.currentPage.match(/^(\d+)/)[1]
     let year = props.currentPage.match(/-(\d+)$/)[1]
     var monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
- 
+    
+    function nextMonth(month, year){
+
+        let newYear = parseInt(month)+1 > 11 ? parseInt(year) + 1 : year
+        let newMonth = parseInt(month)+1 > 11 ? 0 : parseInt(month)+1
+
+        return newMonth + '-' + newYear
+    }
+
+    function lastMonth(month, year){ 
+
+        let newYear = parseInt(month)-1 < 0 ? parseInt(year) -1 : year
+        let newMonth = parseInt(month)-1 < 0 ? 11 : parseInt(month)-1
+
+        return newMonth + '-' + newYear
+    }
+
+
     return (
         <div id="calendar-header">
             <div id="month-control-bar">
-            <div className="button" onClick={()=>props.setPage((parseInt(month)-1)+"-"+year)}>Prev</div>
+            <div className="button" onClick={()=>props.setPage(lastMonth(month,year))}>Prev</div>
             <div> {monthName[month]} {year} </div>
-            <div className="button" onClick={()=>props.setPage((parseInt(month)+1)+"-"+year)}>Next</div>
+            <div className="button" onClick={()=>props.setPage(nextMonth(month,year))}>Next</div>
             </div>
         </div>
     )
